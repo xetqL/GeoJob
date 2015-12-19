3cf9b2167503cc3eb84d6ef193d64c6400f42547
xetqL <anthony.boulmier.cfpt@gmail.com>
2015-12-19 19:44:21 +0100

    add gps

--- GeoJobFinder/app/src/main/java/ch/mse/mobop/geojobfinder/EntryPointActivity.java
+++ GeoJobFinder/app/src/main/java/ch/mse/mobop/geojobfinder/EntryPointActivity.java
@@ -1,13 +1,79 @@
 package ch.mse.mobop.geojobfinder;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.Manifest;
 
 public class EntryPointActivity extends AppCompatActivity {
 
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_point);
+
+        final Button b = (Button) findViewById(R.id.btn_search);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocationManager locationManager = (LocationManager)
+                        getSystemService(Context.LOCATION_SERVICE);
+                String context = Context.LOCATION_SERVICE;
+                locationManager = (LocationManager) getSystemService(context);
+                String provider = locationManager.getBestProvider(new Criteria(), true); // Initial position:
+
+                Log.d("gps", provider);
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, new LocationListener() {
+                            @Override
+                            public void onLocationChanged(Location location) {
+
+                            }
+
+                            @Override
+                            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                            }
+
+                            @Override
+                            public void onProviderEnabled(String provider) {
+
+                            }
+
+                            @Override
+                            public void onProviderDisabled(String provider) {
+
+                            }
+                        });
+                Log.d("location", String.valueOf(locationManager.getLastKnownLocation(provider).getLatitude()));
+
+            }
+
+
+        });
+
     }
+
 }
